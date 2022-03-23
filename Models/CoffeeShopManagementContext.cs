ae7a80e2d24b9251358e7fbb617faab7c860bb08
longlb25 <lelong25200@gmail.com>
2022-03-23 08:36:13 +0700

    update

---
+++ Models/CoffeeShopManagementContext.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace CoffeeShopManagement.Models
+{
+    public partial class CoffeeShopManagementContext : DbContext
+    {
+        public CoffeeShopManagementContext()
+        {
+        }
+
+        public CoffeeShopManagementContext(DbContextOptions<CoffeeShopManagementContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Bill> Bills { get; set; }
+        public virtual DbSet<BillInfo> BillInfos { get; set; }
+        public virtual DbSet<Food> Foods { get; set; }
+        public virtual DbSet<FoodCategory> FoodCategories { get; set; }
+        public virtual DbSet<TableFood> TableFoods { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server =(local); database = CoffeeShopManagement;uid=sa;pwd=1234;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Account>(entity =>
+            {
+                entity.HasKey(e => e.UserName)
+                    .HasName("PK__Account__66DCF95DDC76B8F0");
+
+                entity.ToTable("Account");
+
+                entity.Property(e => e.UserName)
+                    .HasMaxLength(100)
+                    .HasColumnName("userName");
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(100)
+                    .HasColumnName("address");
+
+                entity.Property(e => e.DateOfBirth)
+                    .HasMaxLength(100)
+                    .HasColumnName("dateOfBirth");
+
+                entity.Property(e => e.FullName)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .HasColumnName("fullName");
+
+                entity.Property(e => e.Gender).HasColumnName("gender");
+
+                entity.Property(e => e.PassWord)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .HasColumnName("passWord");
+
+                entity.Property(e => e.PhoneNumber)
+                    .HasMaxLength(100)
+                    .HasColumnName("phoneNumber");
+
+                entity.Property(e => e.Type).HasColumnName("type");
+            });
+
+            modelBuilder.Entity<Bill>(entity =>
+            {
+                entity.ToTable("Bill");
+
+                entity.Property(e => e.BillId).HasColumnName("billId");
+
+                entity.Property(e => e.DateCheckIn)
+                    .HasColumnType("date")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.DateCheckOut).HasColumnType("date");
+
+                entity.Property(e => e.IdTable).HasColumnName("idTable");
+
+                entity.Property(e => e.Status).HasColumnName("status");
+
+                entity.HasOne(d => d.IdTableNavigation)
+                    .WithMany(p => p.Bills)
+                    .HasForeignKey(d => d.IdTable)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Bill__status__300424B4");
+            });
+
+            modelBuilder.Entity<BillInfo>(entity =>
+            {
+                entity.ToTable("BillInfo");
+
+                entity.Property(e => e.BillInfoId).HasColumnName("billInfoId");
+
+                entity.Property(e => e.IdBill).HasColumnName("idBill");
+
+                entity.Property(e => e.IdFood).HasColumnName("idFood");
+
+                entity.Property(e => e.Quantity).HasColumnName("quantity");
+
+                entity.HasOne(d => d.IdBillNavigation)
+                    .WithMany(p => p.BillInfos)
+                    .HasForeignKey(d => d.IdBill)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__BillInfo__quanti__33D4B598");
+
+                entity.HasOne(d => d.IdFoodNavigation)
+                    .WithMany(p => p.BillInfos)
+                    .HasForeignKey(d => d.IdFood)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__BillInfo__idFood__34C8D9D1");
+            });
+
+            modelBuilder.Entity<Food>(entity =>
+            {
+                entity.ToTable("Food");
+
+                entity.Property(e => e.FoodId).HasColumnName("foodId");
+
+                entity.Property(e => e.FoodName)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .HasColumnName("foodName");
+
+                entity.Property(e => e.IdCategory).HasColumnName("idCategory");
+
+                entity.Property(e => e.Price).HasColumnName("price");
+
+                entity.HasOne(d => d.IdCategoryNavigation)
+                    .WithMany(p => p.Foods)
+                    .HasForeignKey(d => d.IdCategory)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Food__price__2B3F6F97");
+            });
+
+            modelBuilder.Entity<FoodCategory>(entity =>
+            {
+                entity.HasKey(e => e.CategoryId)
+                    .HasName("PK__FoodCate__23CAF1D88C78A2DB");
+
+                entity.ToTable("FoodCategory");
+
+                entity.Property(e => e.CategoryId).HasColumnName("categoryId");
+
+                entity.Property(e => e.CategoryName)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .HasColumnName("categoryName");
+            });
+
+            modelBuilder.Entity<TableFood>(entity =>
+            {
+                entity.HasKey(e => e.TableId)
+                    .HasName("PK__TableFoo__5408AD9AB5690DEE");
+
+                entity.ToTable("TableFood");
+
+                entity.Property(e => e.TableId).HasColumnName("tableId");
+
+                entity.Property(e => e.TableName)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .HasColumnName("tableName");
+
+                entity.Property(e => e.TableStatus)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .HasColumnName("tableStatus");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/FoodCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CoffeeShopManagement.Models
+{
+    public partial class FoodCategory
+    {
+        public FoodCategory()
+        {
+            Foods = new HashSet<Food>();
+        }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Food> Foods { get; set; }
+    }
+}
