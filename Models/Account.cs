7a74630a783c182fa73a5dc73601562b7d463a14
longlb25 <lelong25200@gmail.com>
2022-03-23 08:13:20 +0700

    Update

---
+++ Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CoffeeShopManagement.Models
+{
+    public partial class Account
+    {
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string DateOfBirth { get; set; }
+        public bool? Gender { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string PassWord { get; set; }
+        public int Type { get; set; }
+    }
+}
